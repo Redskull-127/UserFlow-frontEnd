95f85818851d00f0375c1a6de79c8cf4041561a4
unissAhmad <uniss159@gmail.com>
2021-09-29 23:17:42 +0530

    photo selection added

---
+++ src/Components/PhotoSection/MehendhiSection.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function MehendhiSection(props) {
+    return (
+        <div>
+            <img src={props.img} alt="Marriage" className="card"/>
+        </div>
+    )
+}
