b8a1a6a3ad7a8703cbf66f59b68dcf7c4c81bda0
Tejas Borkar <76744372+tejas-borkar22@users.noreply.github.com>
2021-10-10 12:09:14 +0530

    Thank You Pop-up added
    Thank You pop-up box added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import HomePage from './Pages/Home/Home'
 import Navbar from './Components/Navbar/Navbar'
 import Footer from './Components/Footer/Footer'
 import NotfoundPage from './Pages/NotfoundPage/NotfoundPage'
+import Thanks from './Components/Thanku_popup/Thanku';
+
 function App() {
   return (
     <Router>
@@ -13,6 +15,7 @@ function App() {
         <Route component={NotfoundPage} />
       </Switch>
       <Footer />
+      <Thanks />
     </Router>
   );
 }
